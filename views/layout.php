caf7b813a4390f67889a454600c7a79b6f20d24a
pekepeke <pekepekesamurai+github@gmail.com>
2015-04-10 17:49:15 +0900

    fixes layout

--- views/layout.php
+++ views/layout.php
@@ -385,6 +385,21 @@
 						<span class="icon-bar"></span>
 					</a>
 					<a class="brand" href="?action=index">Utilities</a>
+					<ul class="nav pills">
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Debug<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="inputtest.php" data-no-pjax="true">Input Test</a></li>
+									<li><a href="status.php" data-no-pjax="true">Status</a></li>
+<!--
+									<li class="divider"></li>
+									<li class="nav-header">Nav header</li>
+									<li><a href="#">Separated link</a></li>
+									<li><a href="#">One more separated link</a></li>
+-->
+								</ul>
+							</li>
+					</ul>
 					<div class="nav-collapse">
 						<ul class="nav">
 							<li<?php if ($__action == "index") echo ' class="active"'; ?>><a href="?action=index">SQL Tools</a></li>
@@ -393,20 +408,6 @@
 							<li<?php if ($__action == "jstools") echo ' class="active"'; ?>><a href="?action=jstools">JS Tools</a></li>
 							<li<?php if ($__action == "easing") echo ' class="active"'; ?>><a href="?action=easing">Easing</a></li>
 							<li<?php if ($__action == "geohash") echo ' class="active"'; ?>><a href="?action=geohash">GeoHash</a></li>
-
-							<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Other<b class="caret"></b></a>
-									<ul class="dropdown-menu">
-										<li><a href="inputtest.php" data-no-pjax="true">Input Test</a></li>
-										<li><a href="status.php" data-no-pjax="true">Status</a></li>
-<!--
-										<li class="divider"></li>
-										<li class="nav-header">Nav header</li>
-										<li><a href="#">Separated link</a></li>
-										<li><a href="#">One more separated link</a></li>
--->
-									</ul>
-								</li>
 <!--
 							<li<?php if ($__action == "test") echo ' class="active"'; ?>><a href="?action=test">Test</a></li>
 							<li><a href="#about">About</a></li>
